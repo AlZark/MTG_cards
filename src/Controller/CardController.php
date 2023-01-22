fc8e90d06276321566132a801fb47a7e12f1923d
Algirdas <alzarkaitis22@gmail.com>
2023-01-22 10:15:21 +0200

    First external api integration

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -5,12 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class CardController extends AbstractController
 {
-    #[Route('api/cards/{id}')]
-    public function getCard($id): Response
-    {
 
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    #[Route('api/cards', methods: ['GET'])]
+    public function getCardList(): Response
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://api.magicthegathering.io/v1/cards',
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+
+        //dd($content);
+
+        return $this->json($response->toArray());
     }
 }
