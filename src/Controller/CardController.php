42eacc68781acf6d8ee23b1cd8b685d65409c2a5
Algirdas <alzarkaitis22@gmail.com>
2023-01-29 12:00:45 +0200

    Frontend improvements, color and search filters

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -3,8 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class CardController extends AbstractController
@@ -17,27 +23,94 @@ class CardController extends AbstractController
         $this->client = $client;
     }
 
-    #[Route('/')]
-    public function homepage(): Response
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    #[Route('cards/', name: 'app_homepage')]
+    public function homepage(Request $request): Response
     {
-        $cards = $this->getCardList()->getContent();
+        $color = $request->query->get('color');
+        $q = $request->query->get('q');
+
+        $cards = $this->getCardList($color, $q)->getContent();
         $data = json_decode($cards, true);
 
         return $this->render('card/homepage.html.twig', [
             'title' => 'MTG Cards',
-            'cards' => $data['cards']
+            'cards' => $data['cards'],
         ]);
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    #[Route('cards/{id}', name: 'app_card_details')]
+    public function cardDetails($id): Response
+    {
 
+        $cards = $this->getCard($id)->getContent();
+        $data = json_decode($cards, true);
 
+        return $this->render('card/details.html.twig', [
+            'title' => 'MTG Cards',
+            'card' => $data['card'],
+        ]);
+    }
 
-    #[Route('api/cards', methods: ['GET'])]
-    public function getCardList(): Response
+    /**
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     */
+    public function getCardList(string $color = null, string $q = null): Response
+    {
+        if (!is_null($color)) {
+            $response = $this->client->request(
+                'GET',
+                'https://api.magicthegathering.io/v1/cards?colorIdentity=' . $color,
+            );
+        } elseif (!is_null($q)) {
+            $response = $this->client->request(
+                'GET',
+                'https://api.magicthegathering.io/v1/cards?name=' . $q,
+            );
+        } else {
+            $response = $this->client->request(
+                'GET',
+                'https://api.magicthegathering.io/v1/cards',
+            );
+        }
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+
+        return $this->json($response->toArray());
+    }
+
+    /**
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     */
+    public function getCard(string $id): Response
     {
         $response = $this->client->request(
             'GET',
-            'https://api.magicthegathering.io/v1/cards',
+            'https://api.magicthegathering.io/v1/cards/' . $id,
         );
 
         $statusCode = $response->getStatusCode();
@@ -45,6 +118,7 @@ class CardController extends AbstractController
         $contentType = $response->getHeaders()['content-type'][0];
         // $contentType = 'application/json'
 
-         return $this->json($response->toArray());
+        return $this->json($response->toArray());
     }
+
 }
