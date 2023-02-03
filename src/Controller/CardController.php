e40ffbddc2214c1c6e22e8cbc8c3a61b9f8524d3
Algirdas <alzarkaitis22@gmail.com>
2023-02-03 17:32:46 +0200

    Renamed templates

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CardController extends AbstractController
 {
 
-    #[Route('cards/', name: 'app_homepage')]
+    #[Route('cards/', name: 'cards')]
     public function getCards(Request $request, CardService $service): Response
     {
         $color = $request->query->get('color');
@@ -22,20 +22,20 @@ class CardController extends AbstractController
 
         $data = json_decode($cards, true);
 
-        return $this->render('card/homepage.html.twig', [
+        return $this->render('card/list.html.twig', [
             'title' => 'MTG Cards',
             'cards' => $data['cards'],
         ]);
     }
 
-    #[Route('cards/{id}', name: 'app_card_details')]
+    #[Route('cards/{id}', name: 'card_view')]
     public function cardDetails($id, CardService $service): Response
     {
         $card = $this->json($service->getCardsById($id)->toArray())->getContent();
 
         $data = json_decode($card, true);
 
-        return $this->render('card/details.html.twig', [
+        return $this->render('card/view.html.twig', [
             'title' => 'MTG Cards',
             'card' => $data['card'],
         ]);
